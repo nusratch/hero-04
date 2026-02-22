a59c4e12ae71bca596b00daa999dfc20f1ed90fc
nusrat <nusratchy.002@gmail.com>
2026-02-23 00:53:45 +0600

    done first header part

---
+++ script/control.js
@@ -0,0 +1,7 @@
+ const total = 8;
+    const interview = 0;
+    const rejected = 0;
+
+    document.getElementById("totalCount").innerText = total;
+    document.getElementById("interviewCount").innerText = interview;
+    document.getElementById("rejectedCount").innerText = rejected;
